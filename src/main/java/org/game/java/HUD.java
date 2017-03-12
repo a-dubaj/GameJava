6457b6569b1649fcc8648b2c6201355e1a7ebba8
Andrzej Dubaj <dubaj.andrzej@gmail.com>
2017-03-13 00:28:41 +0100

    Added Green - Life in life bar

--- src/main/java/org/game/java/HUD.java
+++ src/main/java/org/game/java/HUD.java
@@ -10,12 +10,18 @@ public class HUD {
     public static int HEALTH = 100;
 
     public void tick() {
+        HEALTH --;
 
+        HEALTH = Game.clamp(HEALTH, 0, 100);
     }
 
     public void render(Graphics g) {
         g.setColor(Color.GRAY);
         g.fillRect(15, 15, 200, 32);
+        g.setColor(Color.GREEN);
+        g.fillRect(15, 15, HEALTH = 2, 32);
+        g.setColor(Color.WHITE);
+        g.drawRect(15, 15, 200, 32);
     }
 
 }
