2c7fd495839299276c95dd78f0233828e747252d
Andrzej Dubaj <dubaj.andrzej@gmail.com>
2017-03-13 19:18:28 +0100

    Imptove life bar

--- src/main/java/org/game/java/HUD.java
+++ src/main/java/org/game/java/HUD.java
@@ -9,14 +9,18 @@ public class HUD {
 
     public static int HEALTH = 100;
 
+    private int greenVariable = 255;
+
     public void tick() {
         HEALTH = Game.clamp(HEALTH, 0, 100);
+        greenVariable = Game.clamp(greenVariable, 0, 255);
+        greenVariable = HEALTH *2;
     }
 
     public void render(Graphics g) {
         g.setColor(Color.GRAY);
         g.fillRect(15, 15, 200, 32);
-        g.setColor(Color.GREEN);
+        g.setColor(new Color(75, greenVariable, 0));
         g.fillRect(15, 15, HEALTH * 2, 32);
         g.setColor(Color.WHITE);
         g.drawRect(15, 15, 200, 32);
